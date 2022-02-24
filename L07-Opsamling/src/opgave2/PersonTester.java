52d3bb408e804de3a3a206d0a5f6cdcd12702d7d
AllanFC <allan.christiansen@live.dk>
2022-02-24 23:06:39 +0100

    Now able to take input and print the age of the object created.

--- L07-Opsamling/src/opgave2/PersonTester.java
+++ L07-Opsamling/src/opgave2/PersonTester.java
@@ -1,13 +1,20 @@
 package opgave2;
 
 
+import java.util.Scanner;
+
 public class PersonTester {
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
         opgave2.Person p1 = new opgave2.Person("Ib", 2000, 10, 6);
         p1.printPerson();
+        //System.out.println(p1.leapYear(2066));
+        System.out.println("Input year month day");
+        System.out.println(p1.calcage(sc.nextInt(), sc.nextInt(), sc.nextInt()));
 
-        System.out.println(p1.leapYear(2066));
+
 
     }
 }
