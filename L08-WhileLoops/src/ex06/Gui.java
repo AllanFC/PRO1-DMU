8c94fdcb1719a3f74dbfa3e4794a07ca64af194e
AllanFC <allan.christiansen@live.dk>
2022-02-25 17:23:17 +0100

    Not finished yet!

---
+++ L08-WhileLoops/src/ex06/Gui.java
@@ -0,0 +1,40 @@
+package ex06;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+public class Gui extends Application {
+
+	@Override
+	public void start(Stage stage) {
+		GridPane root = this.initContent();
+		Scene scene = new Scene(root);
+
+		stage.setTitle("Exercise 6");
+		stage.setScene(scene);
+		stage.show();
+	}
+
+	private GridPane initContent() {
+		GridPane pane = new GridPane();
+		Canvas canvas = new Canvas(200, 200);
+		pane.add(canvas, 0, 0);
+		this.drawShapes(canvas.getGraphicsContext2D());
+		return pane;
+	}
+
+	// ------------------------------------------------------------------------
+
+	private void drawShapes(GraphicsContext gc) {
+
+		gc.strokeLine(10, 175, 190, 175);
+
+		while(true){
+			gc.strokeLine(10, 175, 190, 175);
+		}
+	}
+}
