ddc071f4f285a5fb54d0b44ad9ac4a62323fdac9
AllanFC <allan.christiansen@live.dk>
2022-02-25 08:43:27 +0100

    Small print statement to tell user what to input.

--- L06-IfelseScanner/src/ex516/CardApp.java
+++ L06-IfelseScanner/src/ex516/CardApp.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class CardApp {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
+        System.out.println("Input a card notation eg. QH, KD for Queen of hearts and king of diamonds");
         Card mycard = new Card(sc.next());
 
         System.out.println(mycard.getDescription());
